4cd6715034853b7394fd77bcc3d87d09859639f8
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-15 21:11:07 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p className="bg-red-400 text-red-50 font-extrabold">hello</p>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;
