f190c9a254630a056d96b2209fb462454b8e877a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-15 16:53:37 +0600

    checking

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -13,6 +13,7 @@ export default function Signup() {
                 "url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
             }}
           />
+
           <div className="relative z-20 flex items-center text-lg font-medium">
             {/* <img className="h-8" src={} alt="" /> */}
           </div>
