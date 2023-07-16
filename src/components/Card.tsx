5dfa6bcf2273ac6a220b6a6e9b06f4d91b51e319
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-16 09:52:03 +0600

    completed

---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+export default function Card() {
+  return (
+    <div className="my-6 card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          className="h-48"
+          src="https://www.bookgeeks.in/wp-content/uploads/2022/11/The-Art-of-War-by-Sun-Tzu-Book.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import Table from "../components/Table";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
     <>
-      <Table></Table>
+      <div className="py-12">
+        <p className="pb-12 text-center text-3xl font-bold text-lime-950 ">
+          Top Books
+        </p>
+        <div className="px-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
     </>
   );
 }
