f281ac152c65e10c7176a68b124a5450739a0fd6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-16 18:48:20 +0600

    not found completed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="text-red-800 text-center mt-52 font-bold text-2xl">
+    <div className="text-red-900 text-center mt-52 font-bold text-2xl">
       Not Found
     </div>
   );
