09a3fb6ce92e50d4866965946e2f7526ffd35c6e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-16 16:49:42 +0600

    demo signup created

---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,81 @@
+import { useForm } from "react-hook-form";
+
+interface SignupFormInputs {
+  email: string;
+  password: string;
+}
+
+export function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupFormInputs>();
+  //   const dispatch = useAppDispatch();
+  const onSubmit = (data: SignupFormInputs) => {
+    console.log(data);
+    // dispatch(createUser({ email: data.email, password: data.password }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            {/* <Label className="sr-only" htmlFor="email">
+          Email
+        </Label>
+        <Input
+        />
+        <Input
+        />
+        <Input
+        id="password"
+        placeholder="confirm password"
+        type="password"
+        autoCapitalize="none"
+        autoCorrect="off"
+    /> */}
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                {...register("email", { required: "Email is required" })}
+                className="input input-bordered w-full max-w-xs"
+              />
+              {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                id="password"
+                placeholder="your password"
+                type="password"
+                autoCapitalize="none"
+                autoCorrect="off"
+                {...register("password", { required: "Password is required" })}
+                className="input input-bordered w-full max-w-xs"
+              />
+              {errors.password && <p>{errors.password.message}</p>}
+            </div>
+          </div>
+          <button>Create Account</button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,19 +1,10 @@
 import Card from "../components/Card";
+import { SignupForm } from "../components/SignupForm";
 
 export default function Signup() {
   return (
     <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        {/* <Link
-          to="/login"
-          className={cn(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link> */}
-        <button className="btn btn-neutral"></button>
+      <div className="h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div
             className="absolute inset-0 bg-cover"
@@ -39,7 +30,7 @@ export default function Signup() {
                 Enter your email below to create your account
               </p>
             </div>
-            {/* <SignupForm /> */}
+            <SignupForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               {/* <Link
