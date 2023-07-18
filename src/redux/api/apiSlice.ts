ba79cb318534346e7d4d5a703c915c9ade1009a6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-07-18 23:13:08 +0600

    completed

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:7000/api/v1" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalog-server-pi.vercel.app",
+  }),
   tagTypes: ["reviews", "books"],
   endpoints: () => ({}),
 });
